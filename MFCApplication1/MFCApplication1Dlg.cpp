68f51d6f1fe250fe1233d923a685c5705b8a88c5
zzy8200 <zzy8200@gmail.com>
2015-03-14 17:52:01 -0700

    get image from camera

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,161 @@
+﻿
+// MFCApplication1Dlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+#include "CvvImage.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CvCapture* capture;
+CRect rect;
+CDC *pDC;
+HDC hDC;
+CWnd *pwnd;
+// CMFCApplication1Dlg dialog
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CMFCApplication1Dlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication1Dlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CMFCApplication1Dlg::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg message handlers
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+	pwnd = GetDlgItem(IDC_STATIC);
+	//pwnd->MoveWindow(35,30,352,288);  
+	pDC = pwnd->GetDC();
+	//pDC =GetDC();  
+	hDC = pDC->GetSafeHdc();
+	pwnd->GetClientRect(&rect);
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+
+void CMFCApplication1Dlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: Add your message handler code here and/or call default
+	/************************************************************************/
+	/* 显示摄像头                                                           */
+	/************************************************************************/
+	IplImage* m_Frame;
+	m_Frame = cvQueryFrame(capture);
+	CvvImage m_CvvImage;
+	m_CvvImage.CopyOf(m_Frame, 1);
+	if (true)
+	{
+		m_CvvImage.DrawToHDC(hDC, &rect);
+		//cvWaitKey(10);  
+	}
+
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CMFCApplication1Dlg::OnBnClickedButton1()
+{
+	// TODO: Add your control notification handler code here
+	if (!capture)
+	{
+		capture = cvCaptureFromCAM(0);
+		//AfxMessageBox("OK");  
+	}
+
+	if (!capture)
+	{
+		MessageBox(_T("无法打开摄像头"));
+		return;
+	}
+
+	// 测试  
+	IplImage* m_Frame;
+	m_Frame = cvQueryFrame(capture);
+	CvvImage m_CvvImage;
+	m_CvvImage.CopyOf(m_Frame, 1);
+	if (true)
+	{
+		m_CvvImage.DrawToHDC(hDC, &rect);
+		//cvWaitKey(10);  
+	}
+
+	// 设置计时器,每10ms触发一次事件  
+	SetTimer(1, 10, NULL);
+}
+
+
+void CMFCApplication1Dlg::OnBnClickedButton2()
+{
+	// TODO: Add your control notification handler code here
+	cvReleaseCapture(&capture);
+	
+}
---
+++ MFCApplication1/MFCApplication1Dlg.h
@@ -0,0 +1,37 @@
+
+// MFCApplication1Dlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCApplication1Dlg dialog
+class CMFCApplication1Dlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCApplication1Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
